72218252d2d0248af865bb9e9adaba88130f7178
xugr <xugr>
2017-06-08 22:14:35 +0800

    上传测试

--- src/test/java/com/redsun/spring/boot/TestProjectApplicationTests.java
+++ src/test/java/com/redsun/spring/boot/TestProjectApplicationTests.java
@@ -1,16 +1,27 @@
 package com.redsun.spring.boot;
 
+import org.assertj.core.util.Maps;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+import java.util.Map;
+
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
 public class TestProjectApplicationTests {
 
 	@Test
 	public void contextLoads() {
+
+	}
+
+	@Test
+	public void testMapRemove(){
+		Map<String,String> map = Maps.newHashMap("key","value");
+
+		System.out.println(map.remove("key"));
 	}
 
 }
