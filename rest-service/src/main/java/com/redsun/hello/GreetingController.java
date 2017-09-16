817f783ea1b2bfb8984ce73366d0c491042a646c
GloryXu <guangrong_xu@126.com>
2017-09-16 11:10:07 +0800

    spring boot config server code upload.

--- rest-service/src/main/java/com/redsun/hello/GreetingController.java
+++ rest-service/src/main/java/com/redsun/hello/GreetingController.java
@@ -12,8 +12,7 @@ public class GreetingController {
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-        return new Greeting(counter.incrementAndGet(),
-                String.format(template, name));
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
 }
