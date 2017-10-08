54d92007b44d621ea421346f7eaca4f9576d8025
xugr <xugr>
2017-10-08 21:10:28 +0800

    Hystrix 仪表盘，单应用监控实现 （仪表盘一直loading，未解决上传）

--- eureka/eureka-consumer/src/main/java/com/redsun/eureka/consumer/ApplicationConsumer.java
+++ eureka/eureka-consumer/src/main/java/com/redsun/eureka/consumer/ApplicationConsumer.java
@@ -3,11 +3,13 @@ package com.redsun.eureka.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+@EnableCircuitBreaker
 @EnableDiscoveryClient
 @SpringBootApplication(scanBasePackages = "com.redsun")
 public class ApplicationConsumer {
---
+++ hystrix/hystrix-dashboard/src/main/java/com/redsun/hystrix/ApplicationHystrixDashboard.java
@@ -0,0 +1,14 @@
+package com.redsun.hystrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@EnableHystrixDashboard
+@SpringBootApplication
+public class ApplicationHystrixDashboard {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationHystrixDashboard.class, args);
+    }
+}
