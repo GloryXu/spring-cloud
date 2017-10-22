84dea352a39b70afab9b261aa0972125374f683e
xugr <xugr>
2017-10-22 14:11:11 +0800

    config-server注册服务至eureka

--- config-server/src/main/java/com/redsun/application/ConfigServer.java
+++ config-server/src/main/java/com/redsun/application/ConfigServer.java
@@ -2,8 +2,10 @@ package com.redsun.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigServer {
