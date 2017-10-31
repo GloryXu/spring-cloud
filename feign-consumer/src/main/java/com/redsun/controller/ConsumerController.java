1c62958d410379e07c6e95263a92593d8b83c7f4
xugr <xugr>
2017-10-31 22:22:37 +0800

    spring-cloud-feign参数绑定

--- feign-consumer/src/main/java/com/redsun/controller/ConsumerController.java
+++ feign-consumer/src/main/java/com/redsun/controller/ConsumerController.java
@@ -2,10 +2,12 @@ package com.redsun.controller;
 
 import com.redsun.domain.User;
 import com.redsun.service.HelloService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class ConsumerController {
 
@@ -20,10 +22,12 @@ public class ConsumerController {
     @GetMapping(value = "/feign-consumer2")
     public String helloConsumer2() {
         StringBuffer sb = new StringBuffer();
-        sb.append(helloService.hello()).append("\n");
-        sb.append(helloService.hello("XUGR")).append("\n");
-        sb.append(helloService.hello("XUGR", 25)).append("\n");
-        sb.append(helloService.hello(new User("XUGR", 25))).append("\n");
-        return sb.toString();
+        sb.append(helloService.hello()).append(" \n");
+        sb.append(helloService.hello("XUGR")).append(" \n");
+        sb.append(helloService.hello("XUGR", 25)).append(" \n");
+        sb.append(helloService.hello(new User("XUGR", 25))).append(" \n");
+        String retInfo = sb.toString();
+        log.info("retInfo={}", retInfo);
+        return retInfo;
     }
 }
