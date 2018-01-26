c2befcbbfe91cd555cee928c554c82040a7a9a84
xugr <xugr>
2018-01-26 17:25:14 +0800

    修改pom文件，更改项目名称

--- eureka/eureka-consumer/src/main/java/com/redsun/eureka/controller/ConsumerController.java
+++ eureka/eureka-consumer/src/main/java/com/redsun/eureka/controller/ConsumerController.java
@@ -1,6 +1,5 @@
 package com.redsun.eureka.controller;
 
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
